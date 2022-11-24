4469162c94ceb9619f477807b732946ec319a152
nileshparmar0 <78465492+nileshparmar0@users.noreply.github.com>
2022-11-24 19:47:38 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 let screen=document.getElementById('screen');            //Screen is an input tag
 buttons=document.querySelectorAll('button');
-let screenvalue='';                                     //screen value is a string which is to be vacant in the start
+let screenvalue='';                                     //screen value is a string which should be vacant in the start
 for (item of buttons){
     item.addEventListener('click', (e)=>{   
         buttonText = e.target.innerText;               //e.target on the button which is clicked, will give the inner text.
-        console.log('Button text is',buttonText);      //whichever button is clicked, that button will be displayed.  
-        if(buttonText=='X'){                           //X is fancy letter, we need     
+        console.log('Button text is',buttonText);      //whichever button is clicked, that button will be displayed using buttonText  
+        if(buttonText=='X'){                           //X is fancy letter    
             buttonText = '*';           
-            screenvalue += buttonText;                 //using += so we can other numbers as well, not just one no. Screen.value is a vlaue that we need on calc's screen.
+            screenvalue += buttonText;                 //using += so we can add other numbers as well, not just one no. Screen.value is a vlaue that we need on calc's screen.
             screen.value = screenvalue;
         }
         else if (buttonText =='C'){      
